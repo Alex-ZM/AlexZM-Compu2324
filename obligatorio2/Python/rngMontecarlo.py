c5fe2c505d360981729f3e505a7ef409aafc26ee
Alex-ZM <alejandrozarzuela@gmail.com>
2024-03-18 19:51:47 +0100

    creación rngMontecarlo.py

---
+++ obligatorio2/Python/rngMontecarlo.py
@@ -0,0 +1,22 @@
+import random
+import numpy as np
+
+
+def rngVectorParejas(N):  # Genera un vector de N parejas de números aleatorios entre 0 y N
+    vectorParejas = np.random.randint(0, N, (N*N, 2))
+    print(vectorParejas)  # ---- TEMPORAL, BORRAR ESTA LÍNEA MÁS TARDE ----
+    return vectorParejas
+
+
+def rngX():  # Genera un float entre 0 y 1
+    x = random.random()
+    print(x)  # ---- TEMPORAL, BORRAR ESTA LÍNEA MÁS TARDE ----
+    return x
+
+
+# Input del tamaño de la cuadrícula
+N = int(input("\nIntroduce N (tamaño de la cuadrícula): "))
+
+# Generamos los números aleatorios
+rngVectorParejas(N)
+rngX()
