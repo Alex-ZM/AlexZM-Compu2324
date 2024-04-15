99140d72cf6d03672733cde7230779594d97d51f
Alex-ZM <alejandrozarzuela@gmail.com>
2024-04-15 19:58:18 +0200

    trabajo clase 1 Ising

--- obligatorio2/rngMontecarlo.py
+++ obligatorio2/rngMontecarlo.py
@@ -7,19 +7,13 @@ import time
 N = 5  # Dimensión de la cuadrícula
 T = 5  # Temperatura T = [0,5]
 
+s = np.random.choice([-1,+1], size=(N, N))
+print(s)
 
-def rngVectorParejas(N):  # Genera un vector de N parejas de números aleatorios entre 0 y N
-    vectorParejas = np.random.choice([-1,+1], size=(N, N))
-    print(vectorParejas)  # ---- TEMPORAL, BORRAR ESTA LÍNEA MÁS TARDE ----
-    return vectorParejas
-
-
-def rngX():  # Genera un float entre 0 y 1
-    x = random.random()
-    print(x)  # ---- TEMPORAL, BORRAR ESTA LÍNEA MÁS TARDE ----
-    return x
+for n in range(0,N):
+    deltaE = 2*s[n,m]
 
 
-# Generamos los números aleatorios
-rngVectorParejas(N)
-rngX()
+
+
+print(str(s[0,0]))
