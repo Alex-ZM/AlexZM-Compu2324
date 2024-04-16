d9a6fa1580e5a2af86f9b7959ffc5f348d4b83f4
Alex-ZM <alejandrozarzuela@gmail.com>
2024-04-16 20:33:54 +0200

    ising v1

--- obligatorio2/rngMontecarlo.py
+++ obligatorio2/rngMontecarlo.py
@@ -1,58 +1,59 @@
 import random
 import numpy as np
-import time
 
 
 # DEFINICIÓN DE CONSTANTES Y PARÁMETROS
-N = 5  # Dimensión de la cuadrícula
-T = 5  # Temperatura T = [0,5]
-t = 10  # Tiempo
+N = 30    # Dimensión de la cuadrícula
+T = 1.0    # Temperatura T = [0,5]
+t = 1000  # Tiempo
 
 # CREACIÓN DE LA MATRIZ DE ESPINES s
 s = np.random.choice([-1,+1], size=(N,N))
 print(s)
 
+fichero = open("ising_data.dat", "w")
 
 for _ in range(0,t):
 
+    i = random.randint(0,N-1)  # Se elije una partícula aleatoria
+    j = random.randint(0,N-1)  #
 
-    for n in range(0,N):
+    if i==N-1:      #
+        up = i-1    #
+        down = 0    #
+    elif i==0:      #
+        up = N-1    # Periodicidad vertical
+        down = 1    #
+    else:           #
+        up = i-1    #
+        down = i+1  #
+        
+    if j==N-1:       # 
+        left = j-1   # 
+        right = 0    # 
+    elif j==0:       # 
+        left = N-1   # Periodicidad horizontal
+        right = 1    # 
+    else:            # 
+        left = j-1   # 
+        right = j+1  # 
 
-        if n==N:        #
-            up = n-1    #
-            down = 0    #
-        elif n==0:      #
-            up = N      # Periodicidad vertical
-            down = n+1  #
-        else:           #
-            up = n-1    #
-            down = n+1  #
+    deltaE = 2*s[i,j]*(s[up,j]+s[down,j]+s[i,left]+s[i,right])
 
+    if 1<np.exp(-deltaE/T):     # 
+        p = np.exp(-deltaE/T)   # Evaluación de p
+    else:                       # 
+        p = 1                   # 
 
-        for m in range(0,N):
+    n = random.random()  # Número aleatorio entre 0 y 1
 
-            if m==N:         # 
-                left = m-1   # 
-                right = 0    # 
-            elif m==0:       # 
-                left = N     # Periodicidad horizontal
-                right = m+1  # 
-            else:            # 
-                left = m-1   # 
-                right = m+1  # 
+    if n<p:                 # Cambio del espín (i,j)
+        s[i,j] = -s[i,j]    # 
+    
+    # Se guarda el estado de la red en este instante
+    for _ in range(0,N):
+        for i in range(0,N):
+            fichero.write(' '.join(map(str, s[i])) + "\n")
+        fichero.write("\n")
 
-            deltaE = 2*s[n,m]*(s[up,m]+s[down,m]+s[n,left]+s[n,right])
-
-            if 1<np.exp(-deltaE/T):     # 
-                p = np.exp(-deltaE/T)   # Evaluación de p
-            else:                       # 
-                p = 1                   # 
-
-            a = random.random()  # Número aleatorio entre 0 y 1
-
-            if a<p:                 # Cambio del espín (n,m)
-                s[n,m] = -s[n,m]    # 
-
-
-
-# print(str(s[0,0]))
+fichero.close()
