284cb0567ff3ace54280a6c61c04c1b6176c6299
Alex-ZM <alejandrozarzuela@gmail.com>
2024-04-17 18:56:19 +0200

    creacion voluntario2

--- voluntario2/valoresMedios.py
+++ voluntario2/valoresMedios.py
@@ -7,14 +7,13 @@ import time
 # DEFINICIÓN DE CONSTANTES Y PARÁMETROS
 N = 16    # Dimensión de la cuadrícula
 T = 2    # Temperatura T = [0,5]
-t = 10000  # Tiempo
+t = 10**6*N**2  # Tiempo
+
+magnProm = np.array([])
+E = np.array([])
 
 # CREACIÓN DE LA MATRIZ DE ESPINES s
-s = np.random.choice([-1,+1], size=(N,N))
-
-wd = os.path.dirname(__file__)  # Directorio de trabajo
-rd = "ising_data.dat"           # Directorio relativo
-fichero = open(os.path.join(wd,rd), "w")  
+s = np.random.choice([-1,+1], size=(N,N)) 
 
 ini = time.time()
 for _ in range(0,t):
@@ -56,13 +55,40 @@ for _ in range(0,t):
     if n<p:                 # Cambio del espín (i,j)
         s[i,j] = -s[i,j]    # 
     
-    # Se guarda el estado de la red en este instante
-    fichero.write("\n")
-    for i in range(0,N):
-        fichero.write(' '.join(map(str, s[i])) + "\n")
+
+    if t%(N*N*100)==0:  # Cada 100pmc calcula promedios:
+
+        magnProm.append(np.sum(s)/N**2)
+
+        Ei = 0
+        for i in range(0,N):
+            for j in range(0,N):
+                    if i==N-1:      #
+                        up = i-1    #
+                        down = 0    #
+                    elif i==0:      #
+                        up = N-1    # Periodicidad vertical
+                        down = 1    #
+                    else:           #
+                        up = i-1    #
+                        down = i+1  #
+                        
+                    if j==N-1:       # 
+                        left = j-1   # 
+                        right = 0    # 
+                    elif j==0:       # 
+                        left = N-1   # Periodicidad horizontal
+                        right = 1    # 
+                    else:            # 
+                        left = j-1   # 
+                        right = j+1  # 
+
+                    Ei = Ei-0.5*(s[i,j]+(s[up,j]+s[down,j]+s[i,left]+s[i,right]))
+        E.append(Ei)
+
+        
+        
         
 fin = time.time()
 print("|| N = "+str(N)+"\n|| T = "+str(T)+" s\n|| "+str(t)+" iteraciones")
 print("Tiempo de ejecución: "+str(fin-ini))
-
-fichero.close()
