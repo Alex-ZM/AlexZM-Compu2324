cc25c4a9f3869c391fa19745ebd961e7db774943
Alex-ZM <alejandrozarzuela@gmail.com>
2024-05-02 19:37:58 +0200

    schrodinger funcional - conserva norma

--- obligatorio3/animacion_schrodinger.py
+++ obligatorio3/animacion_schrodinger.py
@@ -50,8 +50,8 @@ wd = os.path.dirname(__file__)      # Directorio de trabajo
 rd = "schrodinger_data.dat"               # Directorio relativo
 file_in = os.path.join(wd,rd) # Nombre del fichero de datos
 file_out = "schrodinger" # Nombre del fichero de salida (sin extensión)
-interval = 10 # Tiempo entre fotogramas en milisegundos
-save_to_file = False # False: muestra la animación por pantalla,
+interval = 5 # Tiempo entre fotogramas en milisegundos
+save_to_file = True # False: muestra la animación por pantalla,
                      # True: la guarda en un fichero
 dpi = 150 # Calidad del vídeo de salida (dots per inch)
 
--- obligatorio3/schrodinger.py
+++ obligatorio3/schrodinger.py
@@ -8,19 +8,18 @@ import os
 
 ###################################################################################################################
 
-# DEFINICIÓN DE PARÁMETROS
+# DEFINICIÓN DE PARÁMETROS (Parámetros test: N=200, lmbd=0.5, tiempo = 500)
 N = 400
 nCiclos = int(N/4)
 lmbd = 0.5
-
+tiempo = 500
 skip = 3
 
 k_0 = 2*np.pi*nCiclos/N
 s = 1/(4*k_0**2)
-
-tiempo = 300
 nIter = int(np.floor(tiempo/s))
 
+# VECTOR POTENCIAL
 calc2N5 = int(2*N/5)
 calc3N5 = int(3*N/5)
 Vj = np.zeros(int(calc2N5))
@@ -30,38 +29,38 @@ for i in range(len(VjAux)):
 Vj = np.append(Vj,VjAux)
 Vj = np.append(Vj,np.zeros(N-len(Vj)))
 
-phi = np.zeros(N,dtype=np.complex64)
+###################################################################################################################
+
+# CÁLCULO INICIAL - VECTOR PHI
+phi = np.zeros(N,dtype=np.complex128)
 for j in range(N):
-    phi[j] = np.exp(1j*k_0*j-8*(4*j-N)**2/N**2)
+    phi[j] = np.exp(1j*k_0*j)*np.exp(-8*(4*j-N)**2/N**2)
 phi[0] = 0
 phi[N-1] = 0
 
-gamma = np.zeros(N,dtype=np.complex64)
-alpha = np.zeros(N,dtype=np.complex64)
-A_0 = np.zeros(N,dtype=np.complex64)
+# CÁLCULO INICIAL - ALPHA
+gamma = np.zeros(N,dtype=np.complex128)
+alpha = np.zeros(N,dtype=np.complex128)
+A_0 = np.zeros(N,dtype=np.complex128)
 for j in range(N-1,0,-1):
     A_0[j] = -2+2j/s-Vj[j]
     gamma[j] = 1/(A_0[j]+alpha[j])
     alpha[j-1] = -gamma[j]
 
-b = np.zeros(N,dtype=np.complex64)
-beta = np.zeros(N,dtype=np.complex64)
-ji = np.zeros(N,dtype=np.complex64)
+###################################################################################################################
 
-#def calcBeta():
-#    beta[N-1] = 0
-#    for j in reversed(range(N-1)):
-#        beta[j-1] = gamma[j]*(b[j]-beta[j])
-#    
-#def calcJi():
-#    for j in reversed(range(N-1)):
-#        ji[j] = alpha[j]
+# DEFINICIÓN - VECTORES b, beta, ji
+b = np.zeros(N,dtype=np.complex128)
+beta = np.zeros(N,dtype=np.complex128)
+ji = np.zeros(N,dtype=np.complex128)
 
+# APERTURA FICHERO
 wd = os.path.dirname(__file__)  # Directorio de trabajo
 rd = "schrodinger_data.dat"     # Directorio relativo
 fichero = open(os.path.join(wd,rd), "w")  
-tIni = time.time()
 
+# BUCLE PRINCIPAL - CÁLCULO EVOLUCIÓN TEMPORAL DE PHI
+tIni = time.time()
 for t in range(nIter):
 
     for j in range(N):
@@ -70,24 +69,25 @@ for t in range(nIter):
     beta[N-1] = 0
     for j in range(N-1,1,-1):
         beta[j-1] = gamma[j]*(b[j]-beta[j])
-    beta[0] = 0  # ¿Sobra?
-
-    ji[0] = 0  
+    
+    ji[0] = 0 
     for j in range(N-1):
         ji[j+1] = alpha[j]*ji[j] + beta[j]
-    ji[N-1] = 0  # ¿Sobra?
-
+    
+    # EVOLUCIÓN DE PHI
     for j in range(N):
         phi[j] = ji[j]-phi[j]
 
+    # GUARDA EN EL FICHERO CADA "skip" iteraciones
     if t%skip == 0:
         norma = 0
         for j in range(N):
             fichero.write(str(j)+","+str(np.abs(phi[j]**2))+","+str(Vj[j])+"\n")
-            norma += np.abs(phi[j])
+            norma += np.abs(phi[j]**2)
         fichero.write("\n")
         print(str(norma))
 
 tFin = time.time()
 print("# Tiempo: "+str(tFin-tIni))
+fichero.close()
     
