163a4509aeb95fdf3f2a3ca7d7324a4841850dae
Alex-ZM <alejandrozarzuela@gmail.com>
2024-05-15 18:55:58 +0200

    rungeKutta funciona

--- obligatorio4/animacion_planetas.py
+++ obligatorio4/animacion_planetas.py
@@ -54,10 +54,10 @@ file_in = os.path.join(wd,rd) # Nombre del fichero de datos
 file_out = "planetas" # Nombre del fichero de salida (sin extensión)
 
 # Límites de los ejes X e Y
-x_min = -9
-x_max =  9
-y_min = -9
-y_max =  9
+x_min = -0.2
+x_max =  1.1
+y_min = -0.6
+y_max =  1.4
 
 interval = 1 # Tiempo entre fotogramas en milisegundos
 show_trail = True # Muestra la "estela" del planeta
@@ -69,7 +69,7 @@ dpi = 300 # Calidad del vídeo de salida (dots per inch)
 # Radio del planeta, en las mismas unidades que la posición
 # Puede ser un número (el radio de todos los planetas) o una lista con
 # el radio de cada uno
-planet_radius = 0.05
+planet_radius = 0.002
 #planet_radius = [0.5, 0.7, 1.1]
 
 
--- obligatorio4/rungeKutta.py
+++ obligatorio4/rungeKutta.py
@@ -6,6 +6,8 @@
 import numpy as np
 import time
 import os
+from numba import jit
+import matplotlib.pyplot as plt
 
 ###################################################################################################################
 
@@ -21,26 +23,25 @@ R_L = 1.7374*10**6
 m = 47000
 r = R_T             # Posición inicial del cohete: radio de la Tierra
 phi = 0*np.pi/180  # Latitud desde la que se lanza el cohete
-v = np.sqrt(2*G*M_T/R_T)/d_TL            # Módulo de la velocidad de lanzamiento de la nave (en m/s)
-anguloVelocidad = np.pi/11.6 #0*np.pi/180  # Ángulo de la velocidad de lanzamiento
+v = 0.99999*np.sqrt(2*G*M_T/R_T)/d_TL            # Módulo de la velocidad de lanzamiento de la nave (en m/s)
+anguloVelocidad = 15.455*np.pi/180      # Ángulo de la velocidad de lanzamiento
 phiPunto = 10*np.pi/180  # Velocidad angular de la nave
-#pPhi = m*r**2*phiPunto
 
-nIteraciones = 100000
-h = 0.1
-skip = 500
+nIteraciones = 5000000
+h = 0.05
+skip = 900
+soloUltimoTercio = False
 
 # REESCALADO DE LOS PARÁMETROS
 delta = G*M_T/d_TL**3
 mu = M_L/M_T
 r = r/d_TL
 pr = v*np.cos(anguloVelocidad-phi)
-#pPhi = pPhi/(m*d_TL**2)
 pPhi = r*v*np.sin(anguloVelocidad-phi)
 
 ###################################################################################################################
-
-def rungeKutta(h,r,phi,pr,pPhi,omega,nIteraciones,delta,mu):
+@jit(nopython=True,fastmath=True)
+def rungeKutta(h,r,phi,pr,pPhi,omega,nIteraciones,skip,delta,mu,G,M_T,M_L,d_TL,m):
 
     t = 0
 
@@ -48,21 +49,12 @@ def rungeKutta(h,r,phi,pr,pPhi,omega,nIteraciones,delta,mu):
     k_phi = np.zeros(4)
     k_pr = np.zeros(4)
     k_pPhi = np.zeros(4)
+
+    hamiltoniano = np.zeros(int(nIteraciones/skip)+1)
+
+    posCohete = np.zeros((int(nIteraciones/skip)+1,2))
+    posLuna = np.zeros((int(nIteraciones/skip)+1,2))
     
-    vr = np.zeros(nIteraciones)
-    vphi = np.zeros(nIteraciones)
-    vpr = np.zeros(nIteraciones)
-    vpPhi = np.zeros(nIteraciones)
-
-    hamiltoniano = np.zeros(nIteraciones-1)
-
-    posCohete = np.zeros((nIteraciones,2))
-    posLuna = np.zeros((nIteraciones,2))
-
-    vr[0] = r
-    vphi[0] = phi
-    vpr[0] = pr
-    vpPhi[0] = pPhi
 
     def f_pr(pPhi,r,delta,mu,phi,omega,t):
         rPrima = np.sqrt(1+r**2-2*r*np.cos(phi-omega*t))
@@ -77,38 +69,40 @@ def rungeKutta(h,r,phi,pr,pPhi,omega,nIteraciones,delta,mu):
     
     for j in range(nIteraciones-1):
 
-        k_r[0] = h*vpr[j]
-        k_phi[0] = h*f_phi(vpPhi[j],vr[j])
-        k_pr[0] = h*f_pr(vpPhi[j],vr[j],delta,mu,vphi[j],omega,t)
-        k_pPhi[0] = h*f_pPhi(delta,mu,vr[j],vphi[j],omega,t)
+        k_r[0] = h*pr
+        k_phi[0] = h*f_phi(pPhi,r)
+        k_pr[0] = h*f_pr(pPhi,r,delta,mu,phi,omega,t)
+        k_pPhi[0] = h*f_pPhi(delta,mu,r,phi,omega,t)
 
-        k_r[1] = h*(vpr[j]+k_pr[0]/2)
-        k_phi[1] = h*f_phi(vpPhi[j]+k_pPhi[0]/2,vr[j]+k_r[0]/2)
-        k_pr[1] = h*f_pr(vpPhi[j]+k_pPhi[0]/2,vr[j]+k_r[0]/2,delta,mu,vphi[j]+k_phi[0]/2,omega,t+h/2)
-        k_pPhi[1] = h*f_pPhi(delta,mu,vr[j]+k_r[0]/2,vphi[j]+k_phi[0]/2,omega,t+h/2)
+        k_r[1] = h*(pr+k_pr[0]/2)
+        k_phi[1] = h*f_phi(pPhi+k_pPhi[0]/2,r+k_r[0]/2)
+        k_pr[1] = h*f_pr(pPhi+k_pPhi[0]/2,r+k_r[0]/2,delta,mu,phi+k_phi[0]/2,omega,t+h/2)
+        k_pPhi[1] = h*f_pPhi(delta,mu,r+k_r[0]/2,phi+k_phi[0]/2,omega,t+h/2)
 
-        k_r[2] = h*(vpr[j]+k_pr[1]/2)
-        k_phi[2] = h*f_phi(vpPhi[j]+k_pPhi[1]/2,vr[j]+k_r[1]/2)
-        k_pr[2] = h*f_pr(vpPhi[j]+k_pPhi[1]/2,vr[j]+k_r[1]/2,delta,mu,vphi[j]+k_phi[1]/2,omega,t+h/2)
-        k_pPhi[2] = h*f_pPhi(delta,mu,vr[j]+k_r[1]/2,vphi[j]+k_phi[1]/2,omega,t+h/2)
+        k_r[2] = h*(pr+k_pr[1]/2)
+        k_phi[2] = h*f_phi(pPhi+k_pPhi[1]/2,r+k_r[1]/2)
+        k_pr[2] = h*f_pr(pPhi+k_pPhi[1]/2,r+k_r[1]/2,delta,mu,phi+k_phi[1]/2,omega,t+h/2)
+        k_pPhi[2] = h*f_pPhi(delta,mu,r+k_r[1]/2,phi+k_phi[1]/2,omega,t+h/2)
 
-        k_r[3] = h*(vpr[j]+k_pr[2])
-        k_phi[3] = h*f_phi(vpPhi[j]+k_pPhi[2],vr[j]+k_r[2])
-        k_pr[3] = h*f_pr(vpPhi[j]+k_pPhi[2],vr[j]+k_r[2],delta,mu,vphi[j]+k_phi[2],omega,t+h)
-        k_pPhi[3] = h*f_pPhi(delta,mu,vr[j]+k_r[2],vphi[j]+k_phi[2],omega,t+h)
+        k_r[3] = h*(pr+k_pr[2])
+        k_phi[3] = h*f_phi(pPhi+k_pPhi[2],r+k_r[2])
+        k_pr[3] = h*f_pr(pPhi+k_pPhi[2],r+k_r[2],delta,mu,phi+k_phi[2],omega,t+h)
+        k_pPhi[3] = h*f_pPhi(delta,mu,r+k_r[2],phi+k_phi[2],omega,t+h)
 
 
-        vr[j+1] = vr[j] + (k_r[0] + 2*k_r[1] + 2*k_r[2] + k_r[3])/6
-        vphi[j+1] = vphi[j] + (k_phi[0] + 2*k_phi[1] + 2*k_phi[2] + k_phi[3])/6
-        vpr[j+1] = vpr[j] + (k_pr[0] + 2*k_pr[1] + 2*k_pr[2] + k_pr[3])/6
-        vpPhi[j+1] = vpPhi[j] + (k_pPhi[0] + 2*k_pPhi[1] + 2*k_pPhi[2] + k_pPhi[3])/6
+        r = r + (k_r[0] + 2*k_r[1] + 2*k_r[2] + k_r[3])/6
+        phi = phi + (k_phi[0] + 2*k_phi[1] + 2*k_phi[2] + k_phi[3])/6
+        pr = pr + (k_pr[0] + 2*k_pr[1] + 2*k_pr[2] + k_pr[3])/6
+        pPhi = pPhi + (k_pPhi[0] + 2*k_pPhi[1] + 2*k_pPhi[2] + k_pPhi[3])/6
 
-        posCohete[j,0] = vr[j]*np.cos(vphi[j])
-        posCohete[j,1] = vr[j]*np.sin(vphi[j])
-        posLuna[j,0] = np.cos(omega*t)
-        posLuna[j,1] = np.sin(omega*t)
 
-        hamiltoniano[j] = vpr[j]**2*m*d_TL**2/2 + vpPhi[j]**2*m*d_TL**4/2 - G*m*M_T*d_TL/vr[j] - G*m*M_L*d_TL/(np.linalg.norm(np.subtract(posCohete,posLuna)))
+        if j%skip == 0:
+            posCohete[int(j/skip),0] = r*np.cos(phi)
+            posCohete[int(j/skip),1] = r*np.sin(phi)
+            posLuna[int(j/skip),0] = np.cos(omega*t)
+            posLuna[int(j/skip),1] = np.sin(omega*t)
+
+            hamiltoniano[int(j/skip)] = (pr*m*d_TL)**2/(2*m) + (pPhi*m*d_TL**2)**2/(2*m*(r*d_TL)**2) - G*m*M_T/(r*d_TL) - G*m*M_L/(d_TL*np.linalg.norm(np.subtract(posCohete[int(j/skip)],posLuna[int(j/skip)]))) - omega*pPhi*m*d_TL**2
 
         t = t+h
 
@@ -120,12 +114,20 @@ wd = os.path.dirname(__file__)  # Directorio de trabajo
 rd = "cohete_data.dat"     # Directorio relativo
 fichero = open(os.path.join(wd,rd), "w")  
 
-posCohete,posLuna,hamiltoniano = rungeKutta(h,r,phi,pr,pPhi,omega,nIteraciones,delta,mu)
+tIni = time.time()
+posCohete,posLuna,hamiltoniano = rungeKutta(h,r,phi,pr,pPhi,omega,nIteraciones,skip,delta,mu,G,M_T,M_L,d_TL,m)
+tFin = time.time()
 
-for j in range(nIteraciones):
-    if j%skip == 0:
+if soloUltimoTercio:
+    for j in range(int(2*nIteraciones/(3*skip)),int(nIteraciones/skip)):
+        fichero.write(str(posCohete[j,0]) + "," + str(posCohete[j,1]) + "\n")
+        fichero.write(str(posLuna[j,0]) + "," + str(posLuna[j,1]) + "\n\n")
+else:
+    for j in range(int(nIteraciones/skip)):
         fichero.write(str(posCohete[j,0]) + "," + str(posCohete[j,1]) + "\n")
         fichero.write(str(posLuna[j,0]) + "," + str(posLuna[j,1]) + "\n\n")
 
-print(hamiltoniano[0])
-print(hamiltoniano[nIteraciones-2])
+plt.plot(hamiltoniano)
+plt.show()
+
+print("Tiempo de ejecución: "+str(tFin-tIni))
